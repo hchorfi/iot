9e1adebb9633b7e84026123f9bfe425b26d3ab36
devza <hamza.chorfi.des@gmail.com>
2023-09-30 14:02:36 +0100

    p2 linux updates, ready to push

--- p2/scripts/setup.sh
+++ p2/scripts/setup.sh
@@ -2,15 +2,14 @@
 
 ## setup
 sudo apt-get update
-#sudo mkdir configs
 
 ## install k3s as a server
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--node-external-ip=192.168.56.110' sh -
 
 ## deploy apps
-sudo kubectl apply -f /home/vagrant/configs/app1.yaml
-sudo kubectl apply -f /home/vagrant/configs/app2.yaml
-sudo kubectl apply -f /home/vagrant/configs/app3.yaml
+sudo kubectl apply -f configs/app1.yaml
+sudo kubectl apply -f configs/app2.yaml
+sudo kubectl apply -f configs/app3.yaml
 
-## deploy ingree
-sudo kubectl apply -f /home/vagrant/configs/ingress.yaml
+## deploy ingrees
+sudo kubectl apply -f configs/ingress.yaml
