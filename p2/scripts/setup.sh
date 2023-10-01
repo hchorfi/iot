1a6852d2fce7959f1f29004c55225fb2a99ba216
devza <hamza.chorfi.des@gmail.com>
2023-10-01 07:17:34 +0100

    update configs folder name

--- p2/scripts/setup.sh
+++ p2/scripts/setup.sh
@@ -7,9 +7,9 @@ sudo apt-get update
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip 192.168.56.110" K3S_NODE_NAME="$HOSTNAME" K3S_KUBECONFIG_MODE="644" sh -
 
 ## deploy apps
-sudo kubectl apply -f configs/app1.yaml
-sudo kubectl apply -f configs/app2.yaml
-sudo kubectl apply -f configs/app3.yaml
+sudo kubectl apply -f confs/app1.yaml
+sudo kubectl apply -f confs/app2.yaml
+sudo kubectl apply -f confs/app3.yaml
 
 ## deploy ingrees
-sudo kubectl apply -f configs/ingress.yaml
+sudo kubectl apply -f confs/ingress.yaml
