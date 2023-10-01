55d6bdc0962d140fc3dc1e99e19d78506e502f68
devza <hamza.chorfi.des@gmail.com>
2023-10-01 06:52:11 +0100

    add node-ip

--- p1/scripts/master.sh
+++ p1/scripts/master.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--tls-san 192.168.56.110 --node-external-ip 192.168.56.110" K3S_NODE_NAME="$HOSTNAME" K3S_KUBECONFIG_MODE="644" sh -
-sudo cp /var/lib/rancher/k3s/server/node-token /home/vagrant/configs
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip 192.168.56.110" K3S_NODE_NAME="$HOSTNAME" K3S_KUBECONFIG_MODE="644" sh -
+sudo cp /var/lib/rancher/k3s/server/node-token /home/vagrant/confs
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export TOKEN_FILE="/home/vagrant/configs/node-token"
+export TOKEN_FILE="/home/vagrant/confs/node-token"
 TOKEN=$(cat "$TOKEN_FILE")
 echo "token :", $TOKEN;
-curl -sfL https://get.k3s.io/ | K3S_URL=https://192.168.56.110:6443/ K3S_TOKEN="$TOKEN" sh -
+curl -sfL https://get.k3s.io/ | INSTALL_K3S_EXEC="--node-ip 192.168.56.111" K3S_URL=https://192.168.56.110:6443/ K3S_TOKEN="$TOKEN" sh -
